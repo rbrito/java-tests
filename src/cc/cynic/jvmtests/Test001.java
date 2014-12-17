0d7dd7d3ea402f22244cef44fc103b1f3165e1c1
Rogério Brito <rbrito@ime.usp.br>
2014-12-17 10:22:56 -0200

    jvmtests: test001: Add test for left-to-right evaluation order.

---
+++ src/cc/cynic/jvmtests/Test001.java
@@ -0,0 +1,8 @@
+package cc.cynic.jvmtests;
+
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Strings without parentheses: " + 3 + 4);
+        System.out.println("Strings with parentheses: " + (3 + 4));
+    }
+}
