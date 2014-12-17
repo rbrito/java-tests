98d3fe17930ee0c701765bf3a5af84e9907aea48
Rogério Brito <rbrito@ime.usp.br>
2014-12-17 09:43:03 -0200

    Add first test for doubles.

---
+++ src/cc/cynic/Test000.java
@@ -0,0 +1,17 @@
+package cc.cynic;
+
+public class Test000 {
+    public static void main(String[] args) {
+        double inf = 1.0/0.0;
+        double neginf = -1.0/0.0;
+        double zero = 1.0/inf;
+        double negzero = 1.0/neginf;
+        double nan = 0.0/0.0;
+
+        System.out.println("inf: " + inf);
+        System.out.println("neginf: " + neginf);
+        System.out.println("zero: " + zero);
+        System.out.println("negzero: " + negzero);
+        System.out.println("nan: " + nan);
+    }
+}
