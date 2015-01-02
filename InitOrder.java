8e77d50f2ead329b8491612991b23bbc839d8193
Rogério Brito <rbrito@ime.usp.br>
2015-01-02 06:10:37 -0200

    Add small code to verify initialization order of classes/constructors.

---
+++ InitOrder.java
@@ -0,0 +1,21 @@
+class InitOrder {
+    public static void main(String[] args) {
+        B b = new B(1, 2);
+    }
+}
+
+class A {
+    A() {
+        System.out.println("Initializing A");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("Initializing B");
+    }
+    B(int a, int b) {
+        this();
+        System.out.println("Initializing B(1, 2)");
+    }
+}
