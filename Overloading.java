fae50c848100fe9d4861b6ab2fae9ce43ffa3bad
Rogério Brito <rbrito@ime.usp.br>
2015-01-02 06:14:12 -0200

    Add class to verify choice of overloaded functions.

---
+++ Overloading.java
@@ -0,0 +1,14 @@
+class Overloading {
+    static void f(int a, int b) {
+        System.out.println("Inside the int function.");
+    }
+
+    static void f(double a, double b) {
+        System.out.println("Inside the double function.");
+    }
+
+    public static void main(String[] args) {
+        f(1, 2);
+        f(1, 2.0);
+    }
+}
