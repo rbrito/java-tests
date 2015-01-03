54d1d72cbc2aee802c47469d45b42c4682d2afdd
Rogério Brito <rbrito@ime.usp.br>
2015-01-03 00:24:19 -0200

    NaiveMatrixMultiplication: Reindent.

--- src/NaiveMatrixMultiplication.java
+++ src/NaiveMatrixMultiplication.java
@@ -1,46 +1,46 @@
 import java.util.Random;
 
 class NaiveMatrixMultiplication {
-	static void print(double[][] M, int m, int n, String name) {
-		System.out.println(name + " = [");
+    static void print(double[][] M, int m, int n, String name) {
+        System.out.println(name + " = [");
 
-		for (int i = 0; i < m; i++) {
-			System.out.print("[");
-			for (int j = 0; j < n; j++) {
-				System.out.print(M[i][j] + ", ");
-			}
-			System.out.println("], ");
-		}
-		System.out.println("]");
+        for (int i = 0; i < m; i++) {
+            System.out.print("[");
+            for (int j = 0; j < n; j++) {
+                System.out.print(M[i][j] + ", ");
+            }
+            System.out.println("], ");
+        }
+        System.out.println("]");
 
-	}
+    }
 
-	// We should probably make a Matrix an object
-	public static void main(String[] args) {
-		Random gen = new Random(0); // set seed to obtain deterministic results
+    // We should probably make a Matrix an object
+    public static void main(String[] args) {
+        Random gen = new Random(0); // set seed to obtain deterministic results
 
-		final int m = 4, n = 4, p = 4;
-		double A[][] = new double[m][p];
-		double B[][] = new double[p][n];
-		double C[][] = new double[m][n];
+        final int m = 4, n = 4, p = 4;
+        double A[][] = new double[m][p];
+        double B[][] = new double[p][n];
+        double C[][] = new double[m][n];
 
-		for (int i = 0; i < m; i++)
-			for (int j = 0; j < p; j++)
-				A[i][j] = gen.nextDouble();
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < p; j++)
+                A[i][j] = gen.nextDouble();
 
-		print(A, m, p, "A");
+        print(A, m, p, "A");
 
-		for (int i = 0; i < p; i++)
-			for (int j = 0; j < n; j++)
-				B[i][j] = gen.nextDouble();
+        for (int i = 0; i < p; i++)
+            for (int j = 0; j < n; j++)
+                B[i][j] = gen.nextDouble();
 
-		print(B, p, n, "B");
+        print(B, p, n, "B");
 
-		for (int i = 0; i < m; i++)
-			for (int j = 0; j < n; j++)
-				for (int k = 0; k < p; k++)
-					C[i][j] += A[i][k] * B[k][j];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < p; k++)
+                    C[i][j] += A[i][k] * B[k][j];
 
-		print(C, m, n, "C");
-	}
+        print(C, m, n, "C");
+    }
 }
