47995e0dcf61128c9c98f5a59c685881623a6f3b
Rogério Brito <rbrito@ime.usp.br>
2015-01-03 09:07:49 -0200

    Add the 'Hello, World' of threading.

---
+++ src/ThreadingTest000.java
@@ -0,0 +1,19 @@
+class T extends Thread {
+    int id;
+    T(int i) {
+        id = i;
+    }
+    public void run() {
+        System.out.println("Hi from thread " + id + ".");
+        System.out.println("Bye from thread " + id + ".");
+    }
+}
+
+class ThreadingTest000 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            T t = new T(i);
+            t.start();
+        }
+    }
+}
