31199da1a0017bda1ddc9d149c9eee980dd89701
Rogério Brito <rbrito@ime.usp.br>
2015-01-04 17:15:41 -0200

    ThreadingTest001: Add exercise with join's.
    This is mostly stolen from Dan Grosmann's lecture notes: "A Sophomoric
    Introduction to Shared-Memory Parallelism and Concurrency".

---
+++ src/ThreadingTest001.java
@@ -0,0 +1,58 @@
+import java.lang.Thread;
+
+class SumThread extends Thread {
+    int lo, hi; // fields for communicating inputs
+    int[] arr;
+    int ans = 0; // for communicating output
+
+    SumThread(int[] a, int l, int h) {
+        lo = l; hi = h; arr = a;
+    }
+
+    public void run() { // override from Thread
+        for (int i = lo; i < hi; i++)
+            ans += arr[i];
+    }
+}
+
+class ThreadingTest001 {
+    static int sum(int[] arr) throws InterruptedException {
+        int len = arr.length;
+        int ans = 0;
+
+        // Here we lauch the threads to perform the computation. The number
+        // of threads is hardcoded to be exactly 4 for simplicity.
+        SumThread[] ts = new SumThread[4];
+
+        for (int i = 0; i < 4; i++) {
+            ts[i] = new SumThread(arr, (i*len)/4, ((i+1)*len)/4);
+            ts[i].start();
+        }
+
+        // Here we collect the results
+        for (int i = 0; i < 4; i++) {
+            ts[i].join(); // CRUCIAL to get right
+            ans += ts[i].ans;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final int n = 1000;
+        int[] arr = new int[n];
+
+        // Initialization for dummy test
+        for (int i = 0; i < n; i++)
+            arr[i] = n - i; 
+
+        // Sequential sum
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            ans += arr[i];
+
+        System.out.println("Sequential sum gives: " + ans);
+
+        System.out.println("Threaded sum gives: " + sum(arr));
+    }
+}
