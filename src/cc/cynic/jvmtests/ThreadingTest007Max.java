4d6c931c0d73a8f3e3e2a761c51e601b5467815f
Rogério Brito <rbrito@ime.usp.br>
2015-01-24 00:09:36 -0200

    ThreadingTest007Max: Change to find the max of the array.

--- src/cc/cynic/jvmtests/ThreadingTest007Max.java
+++ src/cc/cynic/jvmtests/ThreadingTest007Max.java
@@ -3,13 +3,13 @@ package cc.cynic.jvmtests;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-class SumArray007 extends RecursiveTask<Integer> {
+class MaxFinder extends RecursiveTask<Integer> {
     static int SEQUENTIAL_CUTOFF = 1000;
 
     int lo, hi; // fields for communicating inputs
     int[] arr;
 
-    SumArray007(int[] a, int l, int h) {
+    MaxFinder(int[] a, int l, int h) {
         lo = l;
         hi = h;
         arr = a;
@@ -17,39 +17,41 @@ class SumArray007 extends RecursiveTask<Integer> {
 
     protected Integer compute() { // override from RecursiveTask
         if (hi - lo <= SEQUENTIAL_CUTOFF) {
-            int ans = 0;
+            int ans = Integer.MIN_VALUE; // "minus infinity" (not good style)
             for (int i = lo; i < hi; i++) {
-                ans += arr[i];
+                if (arr[i] > ans)
+                    ans = arr[i];
             }
             return ans;
         } else {
             // No need for the try-catch block as with Thread
-            SumArray007 left = new SumArray007(arr, lo, (lo + hi) / 2);
-            SumArray007 right = new SumArray007(arr, (lo + hi) / 2, hi);
+            MaxFinder left = new MaxFinder(arr, lo, (lo + hi) / 2);
+            MaxFinder right = new MaxFinder(arr, (lo + hi) / 2, hi);
             left.fork(); // *not* start
             int rightAns = right.compute(); // call `compute` to halve the number of threads
             int leftAns = left.join();
-            return leftAns + rightAns;
+            return (leftAns > rightAns) ? leftAns : rightAns;
         }
     }
 }
 
-class ThreadingTest007 {
+class ThreadingTest007Max {
     // Only one for `ForkJoinPool` the whole program.
     static final ForkJoinPool fjPool = new ForkJoinPool();
 
-    static int sum(int[] arr) {
-        SumArray007 t = new SumArray007(arr, 0, arr.length);
+    static int max(int[] arr) {
+        MaxFinder t = new MaxFinder(arr, 0, arr.length);
         // Note the inclusion of the Task into the Pool.
         return fjPool.invoke(t);
     }
 
-    static int seqsum(int[] arr) {
+    static int seqmax(int[] arr) {
         int len = arr.length;
-        int ans = 0;
+        int ans = Integer.MIN_VALUE; // "minus infinity" (not good style)
 
         for (int i = 0; i < len; i++)
-            ans += arr[i];
+            if (arr[i] > ans)
+                ans = arr[i];
 
         return ans;
     }
@@ -62,7 +64,7 @@ class ThreadingTest007 {
         for (int i = 0; i < n; i++)
             arr[i] = n - i;
 
-        System.out.println("Sequential sum gives: " + seqsum(arr));
-        System.out.println("Threaded sum gives: " + sum(arr));
+        System.out.println("Sequential max gives: " + seqmax(arr));
+        System.out.println("Threaded max gives: " + max(arr));
     }
 }
