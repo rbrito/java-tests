b3f91be2c3317c8dd759606e8942baad81dd8fec
Rogério Brito <rbrito@ime.usp.br>
2015-02-11 05:23:13 -0200

    Rename Test001 to TestAdditionOrder.

--- src/cc/cynic/jvmtests/TestAdditionOrder.java
+++ src/cc/cynic/jvmtests/TestAdditionOrder.java
@@ -1,6 +1,6 @@
 package cc.cynic.jvmtests;
 
-public class Test001 {
+public class TestAdditionOrder {
     public static void main(String[] args) {
         System.out.println("Strings without parentheses: " + 3 + 4);
         System.out.println("Strings with parentheses: " + (3 + 4));
