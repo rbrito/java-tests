c4abc20542847fd0760f5b71fe61d47ca5d37401
Rogério Brito <rbrito@ime.usp.br>
2015-02-12 01:44:34 -0200

    StrongTyping: Add a docstring with the text of the commit msg.

--- src/cc/cynic/jvmtests/StrongTyping.java
+++ src/cc/cynic/jvmtests/StrongTyping.java
@@ -2,6 +2,12 @@ package cc.cynic.jvmtests;
 
 import java.util.Vector;
 
+/**
+ * StrongTyping: Add test to show strong typing in Java.
+ *
+ * Not even what would be allowed as a type promotion in C/C++ (namely, int
+ * to double) is allowed when setting an element of a Vector<Double>.
+ */
 public class StrongTyping {
     public static void main(String[] args) {
         Vector<Double> vd = new Vector<Double>();
