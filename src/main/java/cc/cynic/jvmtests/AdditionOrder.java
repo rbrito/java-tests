051b3bcde17f43bee46a70cd03b4bc3fa533fc7d
Rogério Brito <rbrito@ime.usp.br>
2015-02-17 02:59:45 -0200

    Strip useless Test from name of classes.

--- src/main/java/cc/cynic/jvmtests/AdditionOrder.java
+++ src/main/java/cc/cynic/jvmtests/AdditionOrder.java
@@ -1,6 +1,6 @@
 package cc.cynic.jvmtests;
 
-public class TestAdditionOrder {
+public class AdditionOrder {
     public static void main(String[] args) {
         System.out.println("Strings without parentheses: " + 3 + 4);
         System.out.println("Strings with parentheses: " + (3 + 4));
--- src/main/java/cc/cynic/jvmtests/FloatingPoint.java
+++ src/main/java/cc/cynic/jvmtests/FloatingPoint.java
@@ -1,6 +1,6 @@
 package cc.cynic.jvmtests;
 
-public class TestFloatingPoint {
+public class FloatingPoint {
     public static void main(String[] args) {
         double inf = 1.0 / 0.0;
         double neginf = -1.0 / 0.0;
